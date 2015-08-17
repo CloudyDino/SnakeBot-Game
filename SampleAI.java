51fda30e2706db491a07111e3a38310be33d1a06
Amaan <DrAlias@users.noreply.github.com>
2015-08-17 14:55:22 -0400

    Create SampleAI.java

---
+++ SampleAI.java
@@ -0,0 +1,17 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class SampleAI extends AI {
+    
+    public static final String name = "Sample";
+    public static final int mySnake = 1;
+    public static final int other = 2;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public char getDirection() {
+        //Finish this
+    }
+}
