040043544889a88880aef958237824e5d7599ada
Amaan <CloudyDino@users.noreply.github.com>
2016-08-05 13:53:09 -0400

    Prevent Cheating

--- SnakeBot/Game.java
+++ SnakeBot/Game.java
@@ -1,7 +1,7 @@
 public class Game {
     
-    public static final AI FIRST = new SampleAI1(1); // Replace constructor with Player1's AI
-    public static final AI SECCOND = new SampleAI2(2); // Replace constructor with Player2's AI
+    private static final AI FIRST = new SampleAI1(1); // Replace constructor with Player1's AI
+    private static final AI SECCOND = new SampleAI2(2); // Replace constructor with Player2's AI
     private static final int LENGTH_OF_BOARD = 15;
     private static final int HEIGHT_OF_BOARD = 15;
     private static final int POINTS_TO_WIN = 10;
